7fcc438968d6d6df45894debc382512f21382186
DanteArceneaux <90316637+DanteArceneaux@users.noreply.github.com>
2022-12-01 14:36:27 +0700

    setup server, env, and gitignore

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: "./config/config.env"
+});
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+app.use((req, res, next) => {
+  res.send("Hello World");
+});
+
+// Path: config/config.env
